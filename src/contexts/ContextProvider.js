72f3ff4b1a96db58d424858a23933901a14f9ee6
Shanika Lindsay <shanika.m.lindsay@hotmail.co.uk>
2023-12-01 22:55:27 +0000

    Navbar functionality

--- src/contexts/ContextProvider.js
+++ src/contexts/ContextProvider.js
@@ -12,13 +12,20 @@ const initialState = {
 export const ContextProvider = ({ children }) => {
   const [activeMenu, setActiveMenu] = useState(true);
   const [isClicked, setIsClicked] = useState(initialState);
+  const const [screenSize, setScreenSize] = useState(undefined)
+  const handleClick = (clicked) => {
+    setIsClicked({ ...initialState, [clicked]: true });
+  };
   return (
-    <StateContext.Provider 
-      value={{ 
-        activeMenu, 
-        setActiveMenu 
+    <StateContext.Provider
+      value={{
+        activeMenu,
+        setActiveMenu,
         isClicked,
-        setIsClicked
+        setIsClicked,
+        handleClick,
+        screenSize,
+        setScreenSize
       }}
     >
       {children}
